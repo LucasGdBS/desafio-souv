984ec09516de8c3c15a98091946a647a87554b51
Lucas-PC <lgbs@cesar.school>
2024-10-10 23:48:00 -0300

    cria cardSection

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,16 +9,22 @@ export interface CardProps {
 
 export default function Card({ image, date, title, description }: CardProps) {
   return (
-    <article className="flex flex-col space-y-4">
-      <div>
-        {/* ta 370 por 259 no figma */}
-        <Image src={image} alt={title} />
+    <article className="flex flex-col space-y-4 max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl">
+      <div className="relative">
+        <div className="absolute max-w-[569px] inset-0 bg-gradient-to-br from-pink-500 via-purple-500 to-blue-500 opacity-50 rounded-md z-10" />
+        <Image
+          src={image}
+          alt={title}
+          className="relative rounded-md object-cover max-w-[569px] h-64 md:h-48" 
+          layout="responsive" // Garante que a imagem mantenha a proporção
+        />
       </div>
-      <div className="flex flex-col space-y-4 max-w-96">
+      <div className="flex flex-col space-y-4">
         <p className="font-roboto">{date}</p>
-        <h2 className="text-custom-purple font-bold text-2xl">{title}</h2>
+        <h2 className="text-custom-purple font-bold text-xl sm:text-2xl">{title}</h2>
         <p className="font-roboto">{description}</p>
       </div>
     </article>
   );
 }
+
--- src/components/CardsSection/index.tsx
+++ src/components/CardsSection/index.tsx
@@ -1,18 +1,47 @@
 import Card from "../Card";
-import post2 from "@/assets/post-2.png";
+import post1 from "../../assets/post-1.jpeg";
+import post2 from "../../assets/post-2.jpeg";
+import post3 from "../../assets/post-3.jpeg";
+import post4 from "../../assets/post-4.jpeg";
 
-export default function CardsSection(){
-    return (
-        <div className="bg-white flex justify-between px-32">
-          {Array.from({ length: 3 }, (_, i) => (
-            <Card
-              key={i}
-              date="Janeiro 04, 2022"
-              title="10 dicas para conseguir a vaga desejada"
-              image={post2}
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh nibh eu in aliquet ut adipiscing neque. Sed volutpat aenean sit vitae, sed tristique."
-            />
-          ))}
-        </div>
-    )
+export default function CardsSection() {
+  const posts = [
+    {
+      title: "10 dicas para conseguir a vaga desejada",
+      date: "Janeiro 04, 2022",
+      image: post2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh nibh eu in aliquet ut adipiscing neque. Sed volutpat aenean sit vitae, sed tristique.",
+    },
+    {
+      title: "Deixe seu código mais semântico com essas dicas",
+      date: "Janeiro 04, 2022",
+      image: post3,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh nibh eu in aliquet ut adipiscing neque. Sed volutpat aenean sit vitae, sed tristique.",
+    },
+    {
+      title: "Use essas dicas nas suas aplicações mobile",
+      date: "Janeiro 04, 2022",
+      image: post4,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh nibh eu in aliquet ut adipiscing neque. Sed volutpat aenean sit vitae, sed tristique.",
+    },
+  ];
+
+  return (
+    <div>
+      <section className="bg-white flex flex-col md:flex-row justify-between px-4 md:px-8 lg:px-32 space-x-0 md:space-x-8">
+        {posts.map((post, i) => (
+          <Card
+            key={i}
+            date={post.date}
+            title={post.title}
+            image={post.image}
+            description={post.description}
+          />
+        ))}
+      </section>
+    </div>
+  );
 }
