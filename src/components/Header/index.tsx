e8f639b0948b26fbef25de60850d5b3efd91b5b7
Lucas-PC <lgbs@cesar.school>
2024-10-10 21:58:24 -0300

    ajusta a logo para texto

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
-import img from "./logo.png";
 import SearchBox from "../SearchBox";
 
 export default function Header() {
   return (
     <header className="pt-[64px] px-20 fixed top-0 w-full overflow-x-hidden">
       <div className="flex justify-between items-center">
-        <div>
-          <Image src={img} alt="logo" />
+        <div className="flex text-4xl font-black select-none">
+          <h1 className="text-white">The Blog</h1>
+          <h1 className="text-custom-green">.</h1>
+          {/* Escolhi fazer a logo dessa forma por conta da qualidade da imagem que não ficou muito legal */}
         </div>
         <div className="flex justify-between space-x-8 text-white font-bold">
             <button className="border-l-custom-green border-l-4 pl-1">Home</button>
