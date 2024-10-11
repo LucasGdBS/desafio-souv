ba1b6a094a313189c4a305c7436d52b14ff05637
Lucas-PC <lgbs@cesar.school>
2024-10-10 23:51:58 -0300

    troca a logo para svg

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,21 @@
+import Image from "next/image";
 import SearchBox from "../SearchBox";
+import logo from "./logo.svg";
 
 export default function Header() {
   return (
     <header className="pt-[64px] px-20 fixed top-0 w-full overflow-x-hidden z-50">
       <div className="flex justify-between items-center">
         <div className="flex text-4xl font-black select-none">
-          <h1 className="text-white">The Blog</h1>
-          <h1 className="text-custom-green">.</h1>
-          {/* Escolhi fazer a logo dessa forma por conta da qualidade da imagem que não ficou muito legal */}
+          <Image src={logo} alt="Logo" />
         </div>
         <div className="flex justify-between space-x-8 text-white font-bold">
-            <button className="border-l-custom-green border-l-4 pl-1">Home</button>
-            <button>Sobre</button>
-            <button>Categorias</button>
-            <button>Contato</button>
+          <button className="border-l-custom-green border-l-4 pl-1">
+            Home
+          </button>
+          <button>Sobre</button>
+          <button>Categorias</button>
+          <button>Contato</button>
         </div>
         <div>
           <SearchBox />
